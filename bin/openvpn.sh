4448514e1eab6c44b0c68d8e0ff5b7fd21784836
Steven Hsu <admin@stevenhsu.ca>
2015-06-06 13:50:13 -0700

    Use latest OpenVPN

--- bin/openvpn.sh
+++ bin/openvpn.sh
@@ -10,6 +10,10 @@ if [[ $EUID -ne 0 ]]; then
   exit 1
 fi
 
+# Use latest OpenVPN
+wget -O - https://swupdate.openvpn.net/repos/repo-public.gpg|apt-key add -
+echo "deb http://swupdate.openvpn.net/apt trusty main" > /etc/apt/sources.list.d/swupdate.openvpn.net.list
+
 apt-get update -q
 debconf-set-selections <<EOF
 iptables-persistent iptables-persistent/autosave_v4 boolean true
